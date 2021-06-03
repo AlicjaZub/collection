110d5836aa471da809c8a508db1dc8aee945a0ee
Alicja <puranut@hotmail.com>
2021-06-03 09:46:35 +0100

    working on putting the adding to db into function

--- add_to_db.php
+++ add_to_db.php
@@ -3,20 +3,23 @@
 session_start();
 require_once 'config.php';
 
-$db = connectDB();
-if (isset($_GET)) {
-    $name = $_GET['name'];
-    $latin_name = $_GET['latin_name'];
-    $hardiness = $_GET['hardiness'];
-    $sql = 'INSERT INTO details (`name`, `latin_name`, `hardiness`) VALUES (:name, :latin_name, :hardiness)';
-    $statement = $db->prepare($sql);
-    $statement->execute([
-        ':name' => $name,
-        ':latin_name' => $latin_name,
-        ':hardiness' => $hardiness
-    ]);
-    if ($statement == true) {
-        header('Location: index.php');
+function add_into_db()
+{
+    $db = connectDB();
+    if (isset($_GET)) {
+        $name = $_GET['name'];
+        $latin_name = $_GET['latin_name'];
+        $hardiness = $_GET['hardiness'];
+        $sql = 'INSERT INTO details (`name`, `latin_name`, `hardiness`) VALUES (:name, :latin_name, :hardiness)';
+        $statement = $db->prepare($sql);
+        $statement->execute([
+            ':name' => $name,
+            ':latin_name' => $latin_name,
+            ':hardiness' => $hardiness
+        ]);
+        if ($statement == true) {
+            header('Location: index.php');
+        }
     }
 }
 
